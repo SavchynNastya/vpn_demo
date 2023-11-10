a4554f5837202423e237b7ad5ed3aa6ca5c8cb52
Anastasiia Savchyn <sa@empat.tech>
2023-11-10 15:04:20 +0200

    readme

--- entrypoint-local.sh
+++ entrypoint-local.sh
@@ -28,6 +28,9 @@ pip install -r conf/requirements.txt
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 
+echo "Running migrations..."
+python manage.py migrate
+
 echo "Running server..."
 python manage.py runserver 0.0.0.0:8000
 
